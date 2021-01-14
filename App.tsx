1cf51abe847ab472492aaacc677dfdca7ebdafa1
Moa Hermansson <moa.hermansson@netlight.com>
2021-01-14 23:17:39 +0100

    Added stuff

--- App.tsx
+++ App.tsx
@@ -4,47 +4,61 @@ import {
   StyleSheet,
   Text,
   View,
-  TouchableOpacity,
+  TouchableHighlight,
   TextInput,
+  ImageBackground,
 } from "react-native";
-import logo from "./assets/logo.png";
+import splash from "./assets/background.png";
 
 export default function App() {
   let ranNumber = () => Math.floor(Math.random() * 10) + 1;
 
   const [score, setScore] = React.useState(0);
+  const [pressed, setPressed] = React.useState("pink");
 
   const randomNumberOne = ranNumber();
   const randomNumberTwo = ranNumber();
-  var answers = [
+  const answers = [
     randomNumberOne + randomNumberTwo,
     randomNumberOne + ranNumber(),
     randomNumberTwo + ranNumber(),
   ];
 
+  answers.sort(() => 0.5 - Math.random());
+
   let checkAnswer = (answer: Number) => {
     if (answer === randomNumberOne + randomNumberTwo) {
+      setPressed("pink");
       setScore(score + 1);
     }
   };
 
-  const listItems = answers.map((value) => (
-    <TouchableOpacity onPress={() => checkAnswer(value)} style={styles.button}>
+  const listItems = answers.map((value, index) => (
+    <TouchableHighlight
+      onPress={() => checkAnswer(value)}
+      underlayColor={
+        value === randomNumberOne + randomNumberTwo ? "green" : "red"
+      }
+      style={styles.button}
+      key={"" + value + "" + index}
+    >
       <Text style={styles.buttonText}>{value}</Text>
-    </TouchableOpacity>
+    </TouchableHighlight>
   ));
 
   return (
     <View style={styles.container}>
-      <View style={styles.content}>
-        <Text>Your score: {score}</Text>
-      </View>
-      <View style={styles.content}>
-        <Text style={styles.instructions}>
-          What is {randomNumberOne}+{randomNumberTwo}?
-        </Text>
-      </View>
-      <View style={styles.buttonRow}>{listItems}</View>
+      <ImageBackground source={splash} style={styles.image}>
+        <View style={styles.scoreboard}>
+          <Text>Your score: {score}</Text>
+        </View>
+        <View style={styles.content}>
+          <Text style={styles.instructions}>
+            What is {randomNumberOne}+{randomNumberTwo}?
+          </Text>
+        </View>
+        <View style={styles.buttonRow}>{listItems}</View>
+      </ImageBackground>
     </View>
   );
 }
@@ -52,36 +66,44 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+  },
+  scoreboard: {
+    flex: 3,
+    textAlign: "center",
     alignItems: "center",
     justifyContent: "center",
   },
   content: {
     flex: 3,
-    width: 180,
-    height: 100,
+    textAlign: "center",
     alignItems: "center",
     justifyContent: "center",
   },
   buttonRow: {
-    flex: 4,
+    flex: 3,
     flexDirection: "row",
+    justifyContent: "center",
   },
   logo: {
     width: 80,
     height: 50,
     marginBottom: 10,
   },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
   instructions: {
-    color: "#888",
+    color: "#FFF",
     fontSize: 28,
     marginHorizontal: 15,
   },
   button: {
     backgroundColor: "pink",
-    width: 40,
-    height: 40,
-    margin: 20,
+    width: 85,
+    height: 85,
+    margin: 5,
     borderRadius: 5,
     alignItems: "center",
     justifyContent: "center",
